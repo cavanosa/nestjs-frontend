1a4884a5333a60b4ee7f79d513018e1e9aad5e93
cavanosa <cavanosa@cavanosa.cav>
2020-10-08 14:02:50 +0200

    jwt

--- src/app/auth/registro.component.ts
+++ src/app/auth/registro.component.ts
@@ -1,4 +1,8 @@
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../services/auth.service';
+import { NuevoUsuarioDto } from './../models/nuevo-usuario.dto';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  usuario: NuevoUsuarioDto = null;
+
+  nombre: string;
+  nombreUsuario: string;
+  email: string;
+  password: string;
+
+  constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onRegister(): void {
+    this.usuario = new NuevoUsuarioDto(this.nombre, this.nombreUsuario, this.email, this.password);
+    this.authService.registro(this.usuario).subscribe(
+      data => {
+        this.toastrService.success(data.message, 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center',
+        });
+        this.router.navigate(['/login']);
+      },
+      err => {
+        this.toastrService.error(err.error.message, 'Fail', {
+          timeOut: 3000, positionClass: 'toast-top-center',
+        });
+      }
+    );
   }
 
 }
